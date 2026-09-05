c90f119294568f7c6cc0107890bdb6f65c24b834
Dariusz Pienczykowski <dariusz.pienczykowski@flyrlabs.com>
2026-09-05 21:45:51 +0200

    update fields

--- data/cars/carsChallenge.ts
+++ data/cars/carsChallenge.ts
@@ -58,10 +58,80 @@ const getResults = async () => {
   console.log(results);
 };
 
-getResults()
-  .catch(console.error)
-  .finally(async () => {
-    const client = await initDb();
+const addDataToDb = async () => {
+  const client = await initDb();
+  const collection = client.db("trainingDB").collection("cars");
+  const newCars = [
+    {
+      name: "Car",
+      brand: "Ford",
+      color: "Red",
+    },
+    {
+      name: "Car",
+      brand: "Ford",
+      color: "Blue",
+    },
+  ];
+
+  try {
+    const cars = await collection.insertMany(newCars, { ordered: true });
+    console.log(cars);
+  } catch (error) {
+    console.error(error);
+    await client.close();
+    throw error;
+  }
+};
+
+const getAllCars = async () => {
+  const client = await initDb();
+  const collection = client.db("trainingDB").collection("cars");
+  const cars = await collection.find({}).toArray();
+  console.log(cars, cars.length);
+};
+
+const updateCarFieldsMany = async (name: string, updatedFields: any) => {
+  const client = await initDb();
+  try {
+    const collection = client.db("trainingDB").collection("cars");
+    const cars = await collection.updateMany({ name }, { $set: updatedFields });
+    console.log({ cars }, "updateCarFieldsMany");
+  } catch (error) {
+    console.error(error);
+    await client.close();
+    throw error;
+  }
+};
+
+const updateCarFieldsOne = async (name: string, updatedFields: any) => {
+  const client = await initDb();
+  try {
+    const collection = client.db("trainingDB").collection("cars");
+    const cars = await collection.updateOne({ name }, { $set: updatedFields });
+    console.log({ cars }, "updateCarFieldsOne");
+  } catch (error) {
+    console.error(error);
+    await client.close();
+    throw error;
+  }
+};
+
+const init = async () => {
+  const client = await initDb();
+  try {
+    await getAllCars();
+    await updateCarFieldsMany("Car", { color: "Green", test: 1 });
+    await getAllCars();
+    await updateCarFieldsOne("Car", { color: "Blue", test: 2 });
+    await getAllCars();
+  } catch (error) {
+    console.error(error);
+    await client.close();
+    throw error;
+  } finally {
     await client.close();
     console.log("Closed connection to MongoDB");
-  });
+  }
+};
+init();
